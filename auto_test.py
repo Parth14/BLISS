a6aa314f6244549aa93721208257b2e1e5016947
Parth Behani <parth.behani@gmail.com>
2021-12-08 01:48:39 -0800

    minor changes to auto

--- auto_test.py
+++ auto_test.py
@@ -28,10 +28,13 @@ def Fsail(t,s,cone):
     asaily = asail*math.sin(theta+cone)
     return [s[2], s[3], asunx+asailx, asuny+asaily]
 
-t_vals = [6e7/100 * x for x in range(100)]
-t_sail = [6e6/10 * x for x in range(10)]
-t_year = [3e6/5 * x for x in range(5)]
-angles = [0, 0.5, 1.0, 1.57, 3.0]
+#t_vals = [6e7/100 * x for x in range(100)]
+#t_sail = [6e6/10 * x for x in range(10)]
+t = 3e6
+t_year = [t/5 * x for x in range(5)]
+angles = [0, 0.4, 1.1, 1.57, 2.2]
+#angles = [0, 0.2, 0.4, 0.6, 0.8]
+#angles = [0, 1, 2, 3, 4]
 angles_list = []
 
 for a in angles:
@@ -42,17 +45,14 @@ for a in angles:
                     angles_list.append([a, b, c, d, e])
 
 
-venus = solve_ivp(Fsun, [0, 6e7], [0.72*AU, 0, 0, 35], rtol=1e-8, t_eval=t_vals)
-earth = solve_ivp(Fsun, [0, 6e7], [AU, 0, 0, 30], rtol=1e-8, t_eval=t_vals)
-mars = solve_ivp(Fsun, [0, 6e7], [1.5*AU, 0, 0, 24], rtol=1e-8, t_eval=t_vals)
+
 
 sail_in_x = []
 sail_in_y = []
 count = 0
-# angle = float(input("Enter the initial cone angle: "))
-# increment = abs(float(input("Enter cone angle increment: ")))
 
-prev_sail = solve_ivp(Fsail, [0, 6e7], [AU, 0, 0, 30], rtol=1e-8, args=[0], t_eval=[t_year[0], t_year[1]])
+
+prev_sail = solve_ivp(Fsail, [0, t], [AU, 0, 0, 30], rtol=1e-8, args=[0], t_eval=[t_year[0], t_year[1]])
 sail_in_x.append(prev_sail.y[0][0])
 sail_in_y.append(prev_sail.y[1][0])
 sail_xy_list = []
@@ -61,7 +61,7 @@ count = 0
 for i in angles_list:
     sail_temp = []
     for j in range(len(i)):
-        prev_sail = solve_ivp(Fsail, [0, 6e6], [prev_sail.y[0][1], prev_sail.y[1][1], prev_sail.y[2][1], prev_sail.y[3][1],], rtol=1e-8, args=[i[j]], t_eval=[t_year[0], t_year[1]])
+        prev_sail = solve_ivp(Fsail, [0, t], [prev_sail.y[0][1], prev_sail.y[1][1], prev_sail.y[2][1], prev_sail.y[3][1],], rtol=1e-8, args=[i[j]], t_eval=[t_year[0], t_year[1]])
         sail_temp.append([prev_sail.y[0][0], prev_sail.y[1][0]])
         #sail_temp[j].append(sail_temp[j][2]-1.5*AU)
     #sail_next_year = solve_ivp(Fsail, [0, 6e6], [prev_sail.y[0][1], prev_sail.y[1][1], prev_sail.y[2][1], prev_sail.y[3][1],], rtol=1e-8, args=[j], t_eval=t_year)
@@ -91,62 +91,78 @@ print("steps: ", angles_list[pos])
 print("xy: ", sail_xy_list[pos])
 print("percentage close: ", min_dist*200/AU)
 
-# def keyclick(event):
+"""
+sail_in_x = []
+sail_in_y = []
+count = 0
+angle = float(input("Enter the initial cone angle: "))
+increment = abs(float(input("Enter cone angle increment: ")))
+
+prev_sail = solve_ivp(Fsail, [0, t], [AU, 0, 0, 30], rtol=1e-8, args=[angle], t_eval=t_year)
+sail_in_x.append(prev_sail.y[0][0])
+sail_in_y.append(prev_sail.y[1][0])
+
+venus = solve_ivp(Fsun, [0, t], [0.72*AU, 0, 0, 35], rtol=1e-8, t_eval=t_year)
+earth = solve_ivp(Fsun, [0, t], [AU, 0, 0, 30], rtol=1e-8, t_eval=t_year)
+mars = solve_ivp(Fsun, [0, t], [1.5*AU, 0, 0, 24], rtol=1e-8, t_eval=t_year)
+
+def keyclick(event):
     
-#     global count 
-#     global prev_sail
-#     global angle
-#     global t_vals
+    global count 
+    global prev_sail
+    global angle
+    global t_year
     
-#     if event.key == 'left':
-#         angle -= increment
-#     elif event.key == 'right':
-#         angle += increment
+    if event.key == 'left':
+        angle -= increment
+    elif event.key == 'right':
+        angle += increment
 
-#     print(angle)
-#     current_sail = solve_ivp(Fsail, [0, 6e6], [prev_sail.y[0][1], prev_sail.y[1][1], prev_sail.y[2][1], prev_sail.y[3][1],], rtol=1e-8, args=[angle], t_eval=t_sail)
+    print(angle)
+    current_sail = solve_ivp(Fsail, [0, t], [prev_sail.y[0][1], prev_sail.y[1][1], prev_sail.y[2][1], prev_sail.y[3][1],], rtol=1e-8, args=[angle], t_eval=t_year)
 
-#     sail_in_x.append(current_sail.y[0][0])
-#     sail_in_y.append(current_sail.y[1][0])
+    sail_in_x.append(current_sail.y[0][0])
+    sail_in_y.append(current_sail.y[1][0])
 
-#     plt.clf()
-#     #rectangle to adjust axis
-#     plt.plot([-2.5e8, -2.5e8, 2.5e8, 2.5e8, -2.5e8], [-2.5e8, 2.5e8, 2.5e8, -2.5e8, -2.5e8], color="white")
+    plt.clf()
+    #rectangle to adjust axis
+    plt.plot([-2.5e8, -2.5e8, 2.5e8, 2.5e8, -2.5e8], [-2.5e8, 2.5e8, 2.5e8, -2.5e8, -2.5e8], color="white")
 
-#     #sun
-#     sun = plt.Circle((0, 0), 5e6, color='yellow')
-#     plt.gca().add_patch(sun)
+    #sun
+    sun = plt.Circle((0, 0), 5e6, color='yellow')
+    plt.gca().add_patch(sun)
 
-#     #planets full orbit
-#     plt.plot(earth.y[0], earth.y[1], color="brown")
-#     plt.plot(venus.y[0], venus.y[1], color="brown") 
-#     plt.plot(mars.y[0],  mars.y[1], color="brown") 
+    #planets full orbit
+    plt.plot(earth.y[0], earth.y[1], color="brown")
+    plt.plot(venus.y[0], venus.y[1], color="brown") 
+    plt.plot(mars.y[0],  mars.y[1], color="brown") 
 
-#     #planets current
-#     earth_ball = plt.Circle((earth.y[0][count], earth.y[1][count]), 5e6, color='blue')
-#     plt.gca().add_patch(earth_ball)
+    #planets current
+    earth_ball = plt.Circle((earth.y[0][count], earth.y[1][count]), 5e6, color='blue')
+    plt.gca().add_patch(earth_ball)
 
-#     venus_ball = plt.Circle((venus.y[0][count], venus.y[1][count]), 5e6, color='orange')
-#     plt.gca().add_patch(venus_ball)
+    venus_ball = plt.Circle((venus.y[0][count], venus.y[1][count]), 5e6, color='orange')
+    plt.gca().add_patch(venus_ball)
 
-#     mars_ball = plt.Circle((mars.y[0][count], mars.y[1][count]), 5e6, color='red')
-#     plt.gca().add_patch(mars_ball)
+    mars_ball = plt.Circle((mars.y[0][count], mars.y[1][count]), 5e6, color='red')
+    plt.gca().add_patch(mars_ball)
 
-#     #current sail path
-#     plt.plot(sail_in_x, sail_in_y, color="black") 
-#     sail_ball = plt.Circle((sail_in_x[-1], sail_in_y[-1]), 5e6, color='black')
-#     plt.gca().add_patch(sail_ball)
+    #current sail path
+    plt.plot(sail_in_x, sail_in_y, color="black") 
+    sail_ball = plt.Circle((sail_in_x[-1], sail_in_y[-1]), 5e6, color='black')
+    plt.gca().add_patch(sail_ball)
 
-#     #sail future tracjectory with current angle
-#     plt.plot(current_sail.y[0], current_sail.y[1], color="gray")
+    #sail future tracjectory with current angle
+    #plt.plot(current_sail.y[0], current_sail.y[1], color="gray")
     
-#     plt.draw()
+    plt.draw()
 
-#     prev_sail = current_sail
-#     count += 1
+    prev_sail = current_sail
+    count += 1
 
-# fig,ax=plt.subplots()
-# fig.canvas.mpl_connect('key_press_event',keyclick)
+fig,ax=plt.subplots()
+fig.canvas.mpl_connect('key_press_event',keyclick)
 
-# plt.show()
-# plt.draw()
+plt.show()
+plt.draw()
+"""
